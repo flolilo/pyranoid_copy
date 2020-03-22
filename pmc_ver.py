32821eaf3e654f0735be906df120b1946f933e38
flolilo <30194876+flolilo@users.noreply.github.com>
2020-03-22 16:48:22 +0100

    display git commit ID in version

--- pmc_ver.py
+++ pmc_ver.py
@@ -1,2 +1,19 @@
 # -*- coding: utf-8 -*-
-pmc_version = "py_media-copy v0.0.3"
+from pathlib import Path
+
+
+def get_git_revision(base_path):
+    git_dir = Path(base_path) / '.git'
+    with (git_dir / 'HEAD').open('r') as head:
+        ref = head.readline().split(' ')[-1].strip()
+
+    with (git_dir / ref).open('r') as git_hash:
+        return git_hash.readline().strip()[:7]
+
+
+try:
+    git_ver = get_git_revision("./")
+except Exception:
+    git_ver = "N/A"
+
+pmc_version = "py_media-copy v0.0.3" + " (git: " + str(git_ver) + ")"
