04593716beedac28c09a0082b34be8b1bf739f4f
flolilo <30194876+flolilo@users.noreply.github.com>
2020-03-22 16:55:56 +0100

    forgot to add credit for getting git commit

--- pmc_ver.py
+++ pmc_ver.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 
 
+# CREDIT: https://stackoverflow.com/a/56245722/8013879
 def get_git_revision(base_path):
     git_dir = Path(base_path) / '.git'
     with (git_dir / 'HEAD').open('r') as head:
