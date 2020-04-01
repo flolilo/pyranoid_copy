a32beeaf52dbbb87c08948e75ea9f1a13847fb5b
flolilo <30194876+flolilo@users.noreply.github.com>
2020-04-01 04:08:39 +0200

    delete old preset files

--- pmc_preset.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-default = [source, target, extension_preference, extension_list, source_recurse, source_dedup, source_dedup_tolerance,
-history_dedup, history_path, history_hash, history_write, target_dedup, target_protect, naming_subfolder, naming_file,
-verify, verify_hash, target_compress, unsleep, preset, Verbose,]
-"""
-default = ["", "", 0, "", 1, 0, 1, 1, 1, "", 0, 1, 0, 1, "%y4%-%m%-%d%", "%n%", 1, "MD5", 0, 0, "default", 1]
