943f660d6b47b40c9b0c322f78843c7c4d7bfcd8
flolilo <30194876+flolilo@users.noreply.github.com>
2020-04-13 04:04:17 +0200

    # -*- coding: utf-8 -*- from pathlib import Path
    # CREDIT: https://stackoverflow.com/a/56245722/8013879
    def get_git_revision(base_path):
        try:
            git_dir = Path(base_path) / '.git'
            with (git_dir / 'HEAD').open('r') as head:
                ref = head.readline().split(' ')[-1].strip()
    
            with (git_dir / ref).open('r') as git_hash:
                return git_hash.readline().strip()[:7]
        except Exception:
            return "N/A"
    
    git_ver = get_git_revision("./")
    
    pmc_version = "py_media-copy v0.0.7" + " (git: " + str(git_ver) + ")"

--- pmc_ver.py
+++ pmc_ver.py
@@ -4,17 +4,17 @@ from pathlib import Path
 
 # CREDIT: https://stackoverflow.com/a/56245722/8013879
 def get_git_revision(base_path):
-    git_dir = Path(base_path) / '.git'
-    with (git_dir / 'HEAD').open('r') as head:
-        ref = head.readline().split(' ')[-1].strip()
+    try:
+        git_dir = Path(base_path) / '.git'
+        with (git_dir / 'HEAD').open('r') as head:
+            ref = head.readline().split(' ')[-1].strip()
 
-    with (git_dir / ref).open('r') as git_hash:
-        return git_hash.readline().strip()[:7]
+        with (git_dir / ref).open('r') as git_hash:
+            return git_hash.readline().strip()[:7]
+    except Exception:
+        return "N/A"
 
 
-try:
-    git_ver = get_git_revision("./")
-except Exception:
-    git_ver = "N/A"
+git_ver = get_git_revision("./")
 
 pmc_version = "py_media-copy v0.0.7" + " (git: " + str(git_ver) + ")"
